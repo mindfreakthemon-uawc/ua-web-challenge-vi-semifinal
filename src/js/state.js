771036eae483803532e25876b0b780df461ededa
mindfreakthemon <mfthemon@gmail.com>
2014-05-31 19:49:52 +0300

    Formatter typo

--- src/js/state.js
+++ src/js/state.js
@@ -30,7 +30,7 @@ define(function () {
 		context: context,
 		frequencies: [50, 100, 200, 400, 800, 1500, 2200, 3000, 5000, 7000, 10000, 15000],
 		frequencyFormatter: function (frequency) {
-			return (frequency > 1e3 ? (frequency / 1e3).toFixed(1) + 'M' : frequency) + 'Hz';
+			return (frequency > 1e3 ? (frequency / 1e3).toFixed(1) + 'K' : frequency) + 'Hz';
 		},
 		sourceNode: null,
 		addSource: function (node) {
